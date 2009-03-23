6873e1cc8b5334e33ab2f6558ee4b1cc5fddcea3
sri.panyam <sri.panyam@d968f814-0f58-11de-8837-45bce846067a>
2009-03-23 07:56:56 +0000

    Adding bayeux support.

---
+++ trunk/src/eds/http/bayeuxmodule.cpp
@@ -0,0 +1,24 @@
+//*****************************************************************************
+/*!
+ *  \file   bayeuxmodule.cpp
+ *
+ *  \brief  A module for handling bayeux connections.  Hello Comet!!!
+ *
+ *  \version
+ *      - S Panyam      23/03/2009
+ *        Created
+ *
+ *****************************************************************************/
+
+#include "bayuexmodule.h"
+#include "handlerstage.h"
+#include "request.h"
+#include "response.h"
+
+    //! Called to handle connections
+void SBayeuxModule::ProcessInput(SHttpHandlerData * pHandlerData,
+                                 SHttpHandlerStage *pStage, 
+                                 SBodyPart *        pBodyPart)
+{
+}
+
