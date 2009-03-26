e61995327734b20f5ed111f68e9eb64db9ae0cdd
sri.panyam <sri.panyam@d968f814-0f58-11de-8837-45bce846067a>
2009-03-26 07:11:19 +0000

    Fixing

--- src/eds/http/contentmodule.cpp
+++ src/eds/http/contentmodule.cpp
@@ -59,106 +59,111 @@ void SContentModule::HandleBodyPart(SHttpHandlerData *  pHandlerData,
                                     SBodyPart *         pBodyPart)
 {
     SConnection *pConnection    = pHandlerData->pConnection;
+    SHttpRequest *  pRequest    = pHandlerData->Request();
+    SHttpResponse * pResponse   = pRequest->Response();
     int bpType                  = pBodyPart->Type();
-    std::string boundary;
 
     if (bpType == SBodyPart::BP_OPEN_SUB_MESSAGE)
     {
-        boundary = std::string(pBodyPart->data.begin(), pBodyPart->data.end());
+        // cannot send an open sub message command 
+        // if we dont have multi part messages
+        assert ("Content-Type MUST be multipart" && pResponse->IsMultipart());
+
+        SString boundary(pBodyPart->data.begin(), pBodyPart->data.end());
         pModData->boundaries.push_front(boundary);
-        pBodyPart->bpType = SBodyPart::BP_NORMAL;  // convert to normal message
-        pBodyPart->SetBody("--", 2);
-        pBodyPart->AppendToBody(boundary);
-        pBodyPart->AppendToBody(CRLF, 2);
 
-        // send to next module
-        SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
+        // done with the body part so delete it!
+        delete pBodyPart;
     }
-    else if (bpType == SBodyPart::BP_CLOSE_SUB_MESSAGE && !pModData->boundaries.empty())
+    else if (bpType == SBodyPart::BP_CLOSE_SUB_MESSAGE)
     {
-        boundary = pModData->boundaries.front();
-        pModData->boundaries.pop_front();
-        pBodyPart->bpType = SBodyPart::BP_NORMAL;  // convert to normal message
-        pBodyPart->SetBody("--", 2);
-        pBodyPart->AppendToBody(boundary);
-        pBodyPart->AppendToBody("--", 2);
-        pBodyPart->AppendToBody(CRLF, 2);
+        // cannot send an open sub message command 
+        // if we dont have multi part messages
+        assert ("Content-Type MUST be multipart" && pResponse->IsMultipart());
 
-        // send to next module
-        SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
+        if (!pModData->boundaries.empty())
+        {
+            SString boundary(pModData->boundaries.front());
+            pModData->boundaries.pop_front();
+            pBodyPart->bpType = SBodyPart::BP_NORMAL;  // convert to normal message
+            pBodyPart->SetBody("--", 2);
+            pBodyPart->AppendToBody(boundary);
+            pBodyPart->AppendToBody("--", 2);
+            pBodyPart->AppendToBody(CRLF, 2);
+
+            // send to next module
+            SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
+        }
+        else
+        {
+            // no boundaries so cant send this
+            delete pBodyPart;
+        }
     }
     else if (bpType == SBodyPart::BP_CLOSE_CONNECTION || bpType == SBodyPart::BP_CONTENT_FINISHED)
     {
-        // send to next module so it can close it - nothing to do here
-        SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
-    }
-    else    // treat a normal message
-    {
-        // we have normal messages - so update the contentLength if
-        // necessary and send to next module
-        SHttpRequest *  pRequest    = pHandlerData->Request();
-        SHttpResponse * pResponse   = pRequest->Response();
-        if (!pResponse->IsMultipart())
-        {
-            SHeaderTable &  respHeaders = pResponse->Headers();
-            // append body parts and keep adding to the content length
-            if (pBodyPart)
-            {
-                // append to body (can ignore sub messages as it is single part)
-                int bodySize    = pBodyPart->Size();
-                if (bodySize > 0)
-                {
-                    int contLength = -1;
-                    std::string hdrContLength;
-                    if (respHeaders.HeaderIfExists("Content-Length", hdrContLength))
-                    {
-                        contLength  = atoi(hdrContLength.c_str());
-                    }
+        // Send a whole bunch of Close boundary calls!!
 
-                    // there are 2 issues here... who should be doing the
-                    // caching?  Consider following scenarious:
-                    //
-                    // App sends cont-block1 to this module (block size =
-                    // 10)
-                    //
-                    //      1. contLength is set
-                    //      2. contLength is not set
-                    //
-                    // App now sends cont-block2 to this module (say with
-                    // block size of 20)
-                    //
-                    // What should contLength of the response be?
-                    //
-                    // This is related to who should be doing the caching.
-                    // If this module is doing the caching (for singlepart
-                    // messages) then it can:
-                    //
-                    // set the contlength header only if it is missing (ie
-                    // not explicitly sent by child modules)
-                    //
-                    // or it can cache all body parts for a single part
-                    // message and calculate teh contentLength at the end
-                    // after accumulating all body parts before being sent
-                    // out to the next module
-                    //
-                    // For now do the former - set the contlength header to
-                    // be what ever the body size is and not worry about
-                    // caching.  The application or lower layers MUST do
-                    // the caching.
-                    if (bodySize != contLength)
-                    {
-                        respHeaders.SetIntHeader("Content-Length", bodySize);
-                    }
-                }
+        if (false)
+        {
+            pBodyPart->bpType = SBodyPart::BP_NORMAL;  // convert to normal message
+            pBodyPart->SetBody("");
+
+            while ( ! pModData->boundaries.empty())
+            {
+                SString boundary(pModData->boundaries.front());
+                pModData->boundaries.pop_front();
+
+                pBodyPart->AppendToBody("--", 2);
+                pBodyPart->AppendToBody(boundary);
+                pBodyPart->AppendToBody("--", 2);
+                pBodyPart->AppendToBody(CRLF, 2);
             }
         }
 
-        // TODO: We have to check if the message is for a child message or
-        // the main message - by inspecting the boundary stack.
-        //
-        // TODO: Also the messages must be cached instead of being sent out
-        // if it is a single part message and content length is less than
-        // body length
+        // send to next module so it can close it - nothing to do here
+        SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
+    }
+    else if (pBodyPart)
+    {
+        // we have normal messages - so update the contentLength if
+        // necessary and send to next module
+        if (!pResponse->IsMultipart())
+        {
+            SHeaderTable &  respHeaders = pResponse->Headers();
+            // append to body (can ignore sub messages as it is single part)
+            int bodySize    = pBodyPart->Size();
+            if (bodySize > 0)
+            {
+                int contLength = -1;
+                SString hdrContLength;
+                if (respHeaders.HeaderIfExists("Content-Length", hdrContLength))
+                {
+                    contLength  = atoi(hdrContLength.c_str());
+                }
+
+                // For now set the contlength header to be what ever
+                // the body size is and not worry about caching.  The
+                // application or lower layers MUST do the caching.
+                if (bodySize != contLength)
+                {
+                    respHeaders.SetIntHeader("Content-Length", bodySize);
+                }
+            }
+        }
+        else
+        {
+            // prepend the 'current' boundary and send
+            assert("No boundaries found in multi part message" && !pModData->boundaries.empty());
+
+            SBodyPart *pBoundary = pResponse->NewBodyPart();
+            SString boundary = pModData->boundaries.front();
+            pBoundary->SetBody("--", 2);
+            pBoundary->AppendToBody(boundary);
+            pBoundary->AppendToBody(CRLF, 2);
+            SendBodyPartToModule(pConnection, pStage, pBoundary, pModData, pNextModule);
+        }
+
         std::cerr << "  === ContentModule:HandleBodyPart - " << "Sending Body Part to Next Module" << std::endl;
         SendBodyPartToModule(pConnection, pStage, pBodyPart, pModData, pNextModule);
     }
