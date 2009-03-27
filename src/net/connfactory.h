6a2d1f70105515074e802bf2968e650b35bff125
sri.panyam <sri.panyam@d968f814-0f58-11de-8837-45bce846067a>
2009-03-27 06:33:15 +0000

    Added license

--- src/net/connfactory.h
+++ src/net/connfactory.h
@@ -1,3 +1,28 @@
+//*****************************************************************************
+/*!
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************
+ *
+ *  \file   connfactory.h
+ *
+ *  \brief  Factory for creating connection handlers.
+ *
+ *  \version
+ *      - S Panyam      10/02/2009
+ *        Created
+ *
+ *****************************************************************************/
 
 #ifndef SCONN_FACTORY_H
 #define SCONN_FACTORY_H
--- src/net/server.cpp
+++ src/net/server.cpp
@@ -1,11 +1,25 @@
 //*****************************************************************************
 /*!
- *  \file   tcpserver.cpp
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  \brief
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************
+ *
+ *  \file   server.cpp
+ *
+ *  \brief  Superclass of all multithreaded servers.
  *
  *  \version
- *      - Sri Panyam      10/02/2009
+ *      - S Panyam      10/02/2009
  *        Created
  *
  *****************************************************************************/
